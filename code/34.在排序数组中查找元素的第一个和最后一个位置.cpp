e8ea93a09522f4e7f6c2a7ad91757f1ee27b700f
lg641135360 <641135360@qq.com>
2021-06-25 17:38:09 +0800

    update some solutions

--- code/34.在排序数组中查找元素的第一个和最后一个位置.cpp
+++ code/34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -8,23 +8,60 @@
 class Solution {
 public:
     // 寻找target的下标值的函数尽量不要动，抽象出一个函数，然后使用这个函数解答新问题
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int index1 = target_index(nums,target);
-        int index2 = target_index(nums,target+1) -1; // 巧妙的使用了函数返回该在位置的特性
-        if(index1 < nums.size() && nums[index1] == target) return {index1,index2};
-        else return {-1,-1};        
-    }
+    // vector<int> searchRange(vector<int>& nums, int target) {
+    //     int index1 = target_index(nums,target);
+    //     int index2 = target_index(nums,target+1) -1; // 巧妙的使用了函数返回该在位置的特性
+    //     if(index1 < nums.size() && nums[index1] == target) return {index1,index2};
+    //     else return {-1,-1};        
+    // }
 
     // 函数返回target下标，若无则返回target第一个该在的地方
-    int target_index(vector<int>& nums,int target){
-        int i = 0,j = nums.size() - 1;
-        while(i <= j){
-            int mid = i + (j - i) /2;
-            if(nums[mid] < target) i = mid+1;
-            // 这里将mid不断向前推
-            else j = mid -1;
+    // int target_index(vector<int>& nums,int target){
+    //     int i = 0,j = nums.size() - 1;
+    //     while(i <= j){
+    //         int mid = i + (j - i) /2;
+    //         if(nums[mid] < target) i = mid+1;
+    //         // 这里将mid不断向前推
+    //         else j = mid -1;
+    //     }
+    //     return i;
+    // }
+
+    // 相当于自己实现C++中的lower_bound 和 upper_bound 函数
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if(!nums.size()) return vector<int>{-1,-1};
+        int lower = lower_bound(nums,target);
+        int upper = upper_bound(nums,target) - 1;
+        if(lower == nums.size() || nums[lower] != target)
+            return vector<int>{-1,-1};
+        return vector<int>{lower,upper};
+    }
+
+    // 辅函数 返回第一个出现target值的下标，[) 时候是使用l<r，r应该指向最后一个元素后面
+    int lower_bound(vector<int> &nums, int target) {
+        // 有序数组，查找一个元素当然是选择二分
+        int l = 0,r = nums.size() ,mid;
+        while(l<r){
+            mid = l + (r-l)/2;
+            // 尝试向左搜索一哈
+            if(nums[mid] >= target) r = mid;
+            else l = mid +1;
         }
-        return i;
+        // 最后第一个等于target的位置在l
+        return l;
+    }
+
+    // 辅函数 返回的是 第一个大于待查找数值 出现的位置 （前闭后开）
+    int upper_bound(vector<int> &nums, int target) {
+        int l = 0, r = nums.size() ,mid;
+        while(l<r){
+            mid = l + (r - l) / 2;
+            // 尝试向右搜索
+            if(nums[mid]<=target) l = mid +1;
+            // [l,r)
+            else r = mid;
+        }
+        return l;
     }
 };
 // @lc code=end
--- code/69.x-的平方根.cpp
+++ code/69.x-的平方根.cpp
@@ -10,11 +10,25 @@ public:
     // 抖机灵
     // 出自数值分析中的方法
     // 牛顿迭代法 首先随便猜一个近似值 xx，然后不断令 x 等于 x 和 a/x 的平均数，迭代个六七次后 x 的值就已经相当精确了。
-    int mySqrt(int x) {
-      long r = x;
-      while(r*r > x)
-        r = (r+x/r) /2;
-      return r;
+    // int mySqrt(int x) {
+    //   long r = x;
+    //   while(r*r > x)
+    //     r = (r+x/r) /2;
+    //   return r;
+    // }
+
+    // 二分查找 求f(x) = x^2-a=0的解
+    // 单调函数，观察其函数图像可知：f(0) = 0,f(a) > 0
+    // 在[0,a] 使用二分查找。
+    int mySqrt(int a) {
+        long l = 0, r = a,mid;
+        while(l<=r){
+            mid = l+(r-l)/2;
+            if(mid*mid == a) return mid;
+            else if(mid*mid < a) l = mid+1;
+            else r = mid -1;
+        }
+        return (int)r;
     }
 };
 // @lc code=end
