40002bc768b12ec20d47e44e1e546ad557412696
lg641135360 <641135360@qq.com>
2021-07-16 22:00:06 +0800

    21.7.16later

---
+++ code/199.二叉树的右视图.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=199 lang=cpp
+ *
+ * [199] 二叉树的右视图
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> view;
+        queue<pair<TreeNode*,int>> q;
+
+        if(root) 
+            q.push(make_pair(root,0));
+        
+        while(!q.empty()) {
+            TreeNode *node = q.front().first;
+            int depth = q.front().second;
+            q.pop();
+            if(view.size()==depth) // 该层元素都pop掉，该层最后一个元素
+                view.push_back(node->val);
+            else
+                view[depth] = node->val;  // 这里会刷新该层值
+            if(node->left) 
+                q.push(make_pair(node->left,depth+1));
+            if(node->right)
+                q.push(make_pair(node->right,depth+1));
+        }
+        return view;
+    }
+};
+// @lc code=end
+
