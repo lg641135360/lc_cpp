3a4eaf3dd9c66b73bb352bc62645e70decf8b2a1
lg641135360 <641135360@qq.com>
2021-07-31 20:41:23 +0800

    21.7.31later

--- code/307.区域和检索-数组可修改.cpp
+++ code/307.区域和检索-数组可修改.cpp
@@ -8,15 +8,56 @@
 class NumArray {
 public:
     NumArray(vector<int>& nums) {
-
+        if(nums.size() == 0) return ;
+        int n=nums.size() * 4;
+        for(int i=0;i<n;++i)
+            _value.push_back(0);
+        build_segment_tree(_value,nums,0,0,nums.size()-1);
+        _right_end = nums.size() - 1;
     }
     
-    void update(int index, int val) {
-
+    void update(int i, int val) {
+        update_segment_tree(_value,0,0,_right_end,i,val);
     }
     
-    int sumRange(int left, int right) {
+    int sumRange(int i, int j) {
+        return sum_range_segment_tree(_value,0,0,_right_end,i,j);
+    }
+private:
+    vector<int> _value;
+    int _right_end;
 
+    void build_segment_tree(vector<int> &value,vector<int> &nums,int pos,int left,int right) {
+        if(left == right) {
+            value[pos] = nums[left];
+            return;
+        }
+        int mid = (left+right) /2;
+        build_segment_tree(value,nums,pos*2+1,left,mid);
+        build_segment_tree(value,nums,pos*2+2,mid+1,right);
+        value[pos] = value[pos*2+1] + value[pos*2+2];
+    }
+
+    int sum_range_segment_tree(vector<int> &value,int pos,int left,int right,int qleft,int qright) {
+        if(qleft>right || qright<left)
+            return 0;
+        if(qleft<=left&&qright>=right)
+            return value[pos];
+        int mid = (left+right)/2;
+        return sum_range_segment_tree(value,pos*2+1,left,mid,qleft,qright) + sum_range_segment_tree(value,pos*2+2,mid+1,right,qleft,qright);
+    }
+
+    void update_segment_tree(vector<int> &value,int pos,int left,int right,int index,int new_value) {
+        if(left==right&&left==index) {
+            value[pos] = new_value;
+            return;
+        }
+        int mid=(left+right)/2;
+        if(index<=mid) 
+            update_segment_tree(value,pos*2+1,left,mid,index,new_value);
+        else 
+            update_segment_tree(value,pos*2+2,mid+1,right,index,new_value);
+        value[pos] = value[pos*2+1]+value[pos*2+2];
     }
 };
 
