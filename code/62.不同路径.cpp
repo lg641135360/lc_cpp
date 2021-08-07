48241643fc0d6a32cf0b5ec9120fa336b160ecf8
lg641135360 <641135360@qq.com>
2021-08-07 11:24:56 +0800

    21.8.7early

--- code/62.不同路径.cpp
+++ code/62.不同路径.cpp
@@ -7,12 +7,26 @@
 // @lc code=start
 class Solution {
 public:
+    // dp[i][j]表示从(0,0)位置到(i,j)位置的最大路径个数
     int uniquePaths(int m, int n) {
-
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        // vector<vector<int>> visited(m,vector<int>(n,0));
+        
+        // base case 该位置只能由前一列/前一行得到，只有一种路径
+        for(int i=0;i<m;++i) 
+            dp[i][0] = 1;
+        for(int i=0;i<n;++i)
+            dp[0][i] = 1;
+        
+        for(int i=1;i<m;++i)
+            for(int j=1;j<n;++j)
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                // 这里dp[i][j] 只与dp[i-1][j] + dp[i][j-1]有关
+        return dp[m-1][n-1];
     }
 
-private:
-    vector<vector<int>> directions = {{0,1},[1,0]};
+// private:
+//     vector<vector<int>> directions = {{0,1},[1,0]};
 };
 // @lc code=end
 
--- code/63.不同路径-ii.cpp
+++ code/63.不同路径-ii.cpp
@@ -7,8 +7,46 @@
 // @lc code=start
 class Solution {
 public:
+    // dp[i][j]代表（0，0）位置到（i，j）位置的最多路径数
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,0));
 
+        // base case
+        int index_obs = -1;
+        for(int i=0;i<m;++i) {
+            if(obstacleGrid[i][0]) {
+                index_obs = i;
+                dp[i][0] =0;
+                break;
+            }
+            else dp[i][0] = 1;
+        }
+        if(index_obs != -1) 
+            for(int i=index_obs+1;i<m;++i)
+                dp[i][0] = 0;
+
+        index_obs = -1;
+        for(int i=0;i<n;++i) {
+            if(obstacleGrid[0][i]) {
+                index_obs = i;
+                dp[0][i] =0;
+                break;
+            }
+            else dp[0][i] = 1;
+        }
+        if(index_obs != -1) 
+            for(int i=index_obs+1;i<n;++i)
+                dp[0][i] = 0;
+        
+        for(int i=1;i<m;++i)
+            for(int j=1;j<n;++j) {
+                // dp[i][j] == 0表示不可达
+                if(obstacleGrid[i][j]) dp[i][j] = 0;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        return dp[m-1][n-1];
     }
 };
 // @lc code=end
