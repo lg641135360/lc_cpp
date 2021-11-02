e432da4789b8023cf768356ea7589e32a26a6930
lg641135360 <641135360@qq.com>
2021-11-02 18:51:24 +0800

    update

--- code/121.买卖股票的最佳时机.cpp
+++ code/121.买卖股票的最佳时机.cpp
@@ -7,8 +7,27 @@
 // @lc code=start
 class Solution {
 public:
+    // 只能买卖一次，滑动窗口，左边是最小，右边是最大
     int maxProfit(vector<int>& prices) {
-        
+        // 滑动窗口左边界 begin
+        int begin = 0;
+        int res = 0;
+
+        for(int i=1;i<prices.size();) {
+            // 只有当begin位置大于当前位置，才能移动左边界begin
+            // begin位置要存放当前最小值
+            if(prices[begin] > prices[i]) {
+                begin = i;
+                i++;
+            }
+            else {
+                // 更新收益
+                res = max((prices[i]-prices[begin]),res);
+                i++;
+            }
+
+        }
+        return res;
     }
 };
 // @lc code=end
--- code/228.汇总区间.cpp
+++ code/228.汇总区间.cpp
@@ -7,8 +7,18 @@
 // @lc code=start
 class Solution {
 public:
+    // 滑动窗口遍历即可
     vector<string> summaryRanges(vector<int>& nums) {
-        
+        vector<string> res;
+
+        string tmp;
+        int begin = 0;
+
+        for(int i=1;i<nums.size();++i) {
+            
+        }
+
+        return res;
     }
 };
 // @lc code=end
