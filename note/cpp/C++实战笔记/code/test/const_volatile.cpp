ad1871435d8aa48f8690932802e85d94d66ac9cc
lg641135360 <641135360@qq.com>
2022-03-08 14:11:08 +0800

    update

--- note/cpp/C++实战笔记/code/test/const_volatile.cpp
+++ note/cpp/C++实战笔记/code/test/const_volatile.cpp
@@ -4,10 +4,6 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    const volatile int MAX_LEN = 1024;
-
-    auto ptr = (int *)(&MAX_LEN);
-    *ptr = 2048;
-    cout << MAX_LEN << endl;
-    return 0;
+    hash<int> h;
+    cout << h(1) << endl;
 }
