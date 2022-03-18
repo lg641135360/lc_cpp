c4fe810123eb329f6cc4244d93c30c2fea242a71
lg641135360 <641135360@qq.com>
2022-03-18 21:33:04 +0800

    test

---
+++ code/1299.将每个元素替换为右侧最大元素.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1299 lang=cpp
+ *
+ * [1299] 将每个元素替换为右侧最大元素
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> replaceElements(vector<int>& arr) {
+
+    }
+};
+// @lc code=end
+
---
+++ code/299.猜数字游戏.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=299 lang=cpp
+ *
+ * [299] 猜数字游戏
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    string getHint(string secret, string guess)
+    {
+    }
+};
+// @lc code=end
