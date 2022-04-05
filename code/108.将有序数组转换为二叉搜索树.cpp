2d0e2b7ef488120b45ee0e779fbdcdb233072f86
lg641135360 <641135360@qq.com>
2022-04-05 19:20:23 +0800

    udpate

--- code/108.将有序数组转换为二叉搜索树.cpp
+++ code/108.将有序数组转换为二叉搜索树.cpp
@@ -16,23 +16,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    // 根据中序遍历的序列恢复二叉搜索树
-    // 平衡二叉树，根据中间元素作为根节点建立二叉树
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return dfs(nums,0,nums.size()-1);
+class Solution
+{
+private:
+    TreeNode *traversal(vector<int> &nums, int left, int right)
+    {
+        if (left > right)
+            return nullptr;
+        int mid = left + ((right - left) / 2);
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = traversal(nums, left, mid - 1);
+        root->right = traversal(nums, mid + 1, right);
+        return root;
     }
 
-private:
-    TreeNode* dfs(vector<int>& nums,int start,int end){
-        if(start>end) return {};
-        int mid = start+ (end-start) /2;
-        TreeNode *root= new TreeNode(nums[mid]);
-        root->left = dfs(nums,start,mid-1);
-        root->right = dfs(nums,mid+1,end);
+public:
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+        TreeNode *root = traversal(nums, 0, nums.size() - 1);
         return root;
     }
 };
 // @lc code=end
-
