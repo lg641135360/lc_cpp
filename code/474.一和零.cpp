f57ab3544942de3ba856715a6ca8955371273960
lg641135360 <641135360@qq.com>
2022-06-17 17:19:20 +0800

    test

--- code/474.一和零.cpp
+++ code/474.一和零.cpp
@@ -5,11 +5,27 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
-
+    int findMaxForm(vector<string> &strs, int m, int n)
+    {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (string s : strs)
+        {
+            int oneNum = 0, zeroNum = 0;
+            for (char c : str)
+            {
+                if (c == '0')
+                    zeroNum++;
+                else
+                    oneNum++;
+            }
+            for (int i = m; i >= zeroNum; i--)
+                for (int j = n; j >= oneNum; j--)
+                    dp[i][j] = max(dp[i][j], dp[i - zeroNum][j - oneNum] + 1);
+        }
+        return dp[m][n];
     }
 };
 // @lc code=end
-
