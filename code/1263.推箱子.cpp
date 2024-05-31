8f513f550a8a9c097d8cfb7ad10ca4ecf3439188
lg641135360 <641135360@qq.com>
2024-05-31 23:45:14 +0800

    24.5.31

---
+++ code/1263.推箱子.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1263 lang=cpp
+ *
+ * [1263] 推箱子
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int minPushBox(vector<vector<char>> &grid)
+    {
+    }
+};
+// @lc code=end
